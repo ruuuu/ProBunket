ea0bbb5e2def8ab8e8afb95aa64b7b6781e711d2
ruuuu <rufinka_91@mail.ru>
2020-02-02 18:16:46 +0300

    Update test_ProBunket_admin_edit_hall.py

--- test_ProBunket_admin_edit_hall.py
+++ test_ProBunket_admin_edit_hall.py
@@ -11,7 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains # lля сколддинга к нужному элементу импортируем класс ActionChains
 
-import pytest
+#import pytest
 
 
 from random import randint
